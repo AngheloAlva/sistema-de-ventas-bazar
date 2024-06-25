c0cddaa9594ff4c3dad71c78de375e5c311cac4f
Anghelo Alva <ance.anghelo@gmail.com>
2024-06-25 11:43:42 -0400

    Update sidebar items

--- app/components/sideMenu/SideMenu.tsx
+++ app/components/sideMenu/SideMenu.tsx
@@ -1,12 +1,12 @@
-import { Home, Package, Package2, PanelLeft, ShoppingCart, Users2 } from "lucide-react"
 import { Sheet, SheetContent, SheetTrigger } from "@/app/components/ui/sheet"
-import UserDropDown from "./UserDropDown"
+import { Home, Package, Package2, PanelLeft } from "lucide-react"
 import SideMenuItem from "./SideMenuItem"
+import UserDropDown from "./UserDropdown"
 import { Button } from "../ui/button"
 
 export default function Header(): React.ReactElement {
 	return (
-		<header className="bg-background sticky top-0 z-30 flex h-14 items-center gap-4 border-b px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+		<header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
 			<Sheet>
 				<SheetTrigger asChild>
 					<Button size="icon" variant="outline" className="sm:hidden">
@@ -16,24 +16,14 @@ export default function Header(): React.ReactElement {
 				</SheetTrigger>
 				<SheetContent side="left" className="sm:max-w-xs">
 					<nav className="grid gap-6 text-lg font-medium">
-						<div className="bg-primary text-primary-foreground flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full text-lg font-semibold md:text-base">
+						<div className="flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base">
 							<Package2 className="h-5 w-5 transition-all" />
 						</div>
 						<SideMenuItem href="/" icon={<Home className="h-5 w-5" />} label="Dashboard" />
 						<SideMenuItem
-							href="/orders"
-							icon={<ShoppingCart className="h-5 w-5" />}
-							label="Orders"
-						/>
-						<SideMenuItem
-							href="/products"
+							href="/productos"
 							icon={<Package className="h-5 w-5" />}
-							label="Products"
-						/>
-						<SideMenuItem
-							href="/customers"
-							icon={<Users2 className="h-5 w-5" />}
-							label="Customers"
+							label="Productos"
 						/>
 					</nav>
 				</SheetContent>
