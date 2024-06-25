4f7db2b3890e597ddc3449229d29327678125dbc
Anghelo Alva <ance.anghelo@gmail.com>
2024-06-25 11:46:30 -0400

    Add sidebar to layout and update sidebar items

---
+++ app/(pages)/productos/page.tsx
@@ -0,0 +1,3 @@
+export default function ProductPage(): React.ReactElement {
+	return <div></div>
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { TooltipProvider } from "./components/ui/tooltip"
 import { inter } from "./config/fonts"
 import type { Metadata } from "next"
 import "./globals.css"
+import { Aside, SideMenu } from "./components/sideMenu"
 
 export const metadata: Metadata = {
 	title: "Los monitos de la Nona",
@@ -16,7 +17,15 @@ export default function RootLayout({
 	return (
 		<html lang="es">
 			<body className={inter.className}>
-				<TooltipProvider>{children}</TooltipProvider>
+				<TooltipProvider>
+					<div className="flex min-h-screen w-full flex-col bg-muted/40">
+						<Aside />
+						<div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+							<SideMenu />
+							{children}
+						</div>
+					</div>
+				</TooltipProvider>
 			</body>
 		</html>
 	)
